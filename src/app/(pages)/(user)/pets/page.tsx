d55fca4539e316d4292c7ca6274d4e96d6be7c24
manhdoanh04 <libyty97@gmail.com>
2025-03-23 11:21:25 +0700

    update page2

--- src/app/(pages)/(user)/pets/page.tsx
+++ src/app/(pages)/(user)/pets/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Head from 'next/head';
+
 
 import { Section1 } from "./section1";
 import { Section2 } from "./section2";
@@ -12,9 +12,7 @@ export default  function Petlist()
 return(
   <>
 
-<Head>
-        <title>Danh sách sản phẩm</title>
-      </Head>
+
 <UserLayout>
  <Section1/>
  <Section2/>
